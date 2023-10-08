b260eb58b190e8be4068a8dcd0b4b9cb805fdbf7
rudrodip <official.rudrodipsarker@gmail.com>
2023-10-08 17:32:17 +0600

    improved loading

--- src/components/chat/chat-window.tsx
+++ src/components/chat/chat-window.tsx
@@ -1,11 +1,26 @@
 "use client";
 
 import "@/styles/chat.css";
-import { Text, X } from "lucide-react";
+import dynamic from "next/dynamic";
 import { Button } from "@/components/ui/button";
-import { Chat } from "./chat";
 import { useAppSelector, useAppDispatch } from "@/redux/hooks";
 import { toggleChat } from "@/redux/slices/chatSlice";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Card } from "@/components/ui/card";
+
+const DynamicChat = dynamic(() => import("./chat").then((mod) => mod.Chat), {
+  loading: () => {
+    return (
+      <Card className="w-[90vw] lg:w-[40vw]">
+        <Skeleton className="h-[20vh] lg:h-[22vh]"></Skeleton>
+        <Skeleton className="h-[40vh] lg:h-[42vh] mt-5"></Skeleton>
+        <div>
+          <Skeleton className="h-12 w-full mt-5"></Skeleton>
+        </div>
+      </Card>
+    );
+  },
+});
 
 const ChatWindow = () => {
   const dispatch = useAppDispatch();
@@ -17,14 +32,13 @@ const ChatWindow = () => {
 
   return (
     <div className="fixed bottom-8 right-8 z-50">
+      {isOpen && <DynamicChat initialMessages={messages} />}
       <div className="flex items-center justify-end">
         <Button
-          className="chat w-14 h-14 mb-2 border-primary cursor-pointer rounded-full"
+          className="chat w-14 h-14 mb-2 rounded-full"
           onClick={handleToggleChat}
-        >
-        </Button>
+        ></Button>
       </div>
-      {isOpen && <Chat initialMessages={messages} />}
     </div>
   );
 };
