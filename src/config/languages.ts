c3672f0306ad657c6690cc1c718da81004c626ac
rudrodip <official.rudrodipsarker@gmail.com>
2023-10-08 17:33:29 +0600

    languages config

---
+++ src/config/languages.ts
@@ -0,0 +1,13 @@
+import type { labelValuePair } from '@/types'
+
+export const languages: labelValuePair[] = [
+  { value: "Python", label: "Python" },
+  { value: "Java", label: "Java" },
+  { value: "C++", label: "C++" },
+  { value: "C#", label: "C#" },
+  { value: "Ruby", label: "Ruby" },
+  { value: "Swift", label: "Swift" },
+  { value: "Go", label: "Go" },
+  { value: "Solidity", label: "Solidity" },
+  { value: "Dart", label: "Dart" },
+]
