4106e2d89f890eb1133a2c58d8ca30441969dbcd
rudrodip <official.rudrodipsarker@gmail.com>
2023-10-08 17:30:08 +0600

    initial page

---
+++ src/app/(dashboard)/messages/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { db } from "@/lib/db";
+import { getCurrentUser } from "@/lib/session";
+
+
+export default function Page(){
+  return (
+    <section>
+      messages
+    </section>
+  )
+}
