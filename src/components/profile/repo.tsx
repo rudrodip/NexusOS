2d9795faef0872745bb52fef6b97f28bc9cc6cf9
rudrodip <official.rudrodipsarker@gmail.com>
2023-10-08 17:32:50 +0600

    style changed

--- src/components/profile/repo.tsx
+++ src/components/profile/repo.tsx
@@ -27,48 +27,46 @@ export const Repo = ({
   authorUsername,
 }: RepoProps) => {
   return (
-    <div className="p-1 rounded-md border hover:bg-primary delay-75 duration-150">
-      <div className="flex h-full w-auto items-center p-3 gap-3 rounded-md bg-secondary text-sm">
-        <a href={`https://github.com/${authorUsername}`} target="_blank">
-          <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Avatar>
-                  <AvatarImage
-                    alt="Picture"
-                    src={`https://avatars.githubusercontent.com/${authorUsername}`}
-                  />
-                  <AvatarFallback>
-                    <span className="sr-only">{authorUsername}</span>
-                    <Icons.user className="h-4 w-4" />
-                  </AvatarFallback>
-                </Avatar>
-              </TooltipTrigger>
-              <TooltipContent>
-                <p>{authorUsername}</p>
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-        </a>
-        <Link
-          href={{
-            pathname: `/projects/${authorUsername}/${name}`,
-          }}
-          className="flex flex-col justify-between"
-        >
-          <div>
-            <a href={repoUrl} target="_blank" className="font-heading">
-              {name}
-            </a>
-            <p>{desc}</p>
-            <p>{language}</p>
-          </div>
-          <p className="flex gap-2 items-center justify-start my-1">
-            <Star className="w-4 h-4" />
-            {starCount}
-          </p>
-        </Link>
-      </div>
+    <div className="flex h-full w-auto items-center p-3 gap-3 rounded-md border-2 hover:border-primary delay-75 duration-100 text-sm">
+      <a href={`https://github.com/${authorUsername}`} target="_blank">
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Avatar>
+                <AvatarImage
+                  alt="Picture"
+                  src={`https://avatars.githubusercontent.com/${authorUsername}`}
+                />
+                <AvatarFallback>
+                  <span className="sr-only">{authorUsername}</span>
+                  <Icons.user className="h-4 w-4" />
+                </AvatarFallback>
+              </Avatar>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>{authorUsername}</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </a>
+      <Link
+        href={{
+          pathname: `/projects/${authorUsername}/${name}`,
+        }}
+        className="flex flex-col justify-between"
+      >
+        <div>
+          <a href={repoUrl} target="_blank" className="font-heading">
+            {name}
+          </a>
+          <p>{desc}</p>
+          <p>{language}</p>
+        </div>
+        <p className="flex gap-2 items-center justify-start my-1">
+          <Star className="w-4 h-4" />
+          {starCount}
+        </p>
+      </Link>
     </div>
   );
 };
